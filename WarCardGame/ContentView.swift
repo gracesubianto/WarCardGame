8c3a6369bd7ae4f00d9c3cae67ae3201207bf02d
gracesubianto <gracesubianto@gmail.com>
2020-05-12 20:28:57 +1200

    12/05/20 8:28pm

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
     @State private var randNum1 = 2
     @State private var randNum2 = 2
     
+    @State private var score1 = 0
+    @State private var score2 = 0
+    
     var body: some View {
         ZStack{
             Image("background")
@@ -41,6 +44,16 @@ struct ContentView: View {
                     self.randNum1 = Int.random(in: 2...14)
                     self.randNum2 = Int.random(in: 2...14)
                     
+                    //update scores
+                    if self.randNum1 > self.randNum2
+                    {
+                        self.score1 += 1
+                    }
+                    else if self.randNum2 > self.randNum1
+                    {
+                        self.score2 += 1
+                    }
+                    
                 }) {
                     Image("dealbutton").renderingMode(.original)
                 }
@@ -49,12 +62,12 @@ struct ContentView: View {
                 HStack{
                     VStack{
                         Text("player score").bold().padding(.bottom, 20)
-                        Text("0")
+                        Text(String(score1))
                     }.padding(.leading, 20).foregroundColor(.white)
                     Spacer()
                     VStack{
                         Text("cpu score")
-                        Text("0")
+                        Text(String(score2))
                         
                     }.padding(.trailing, 20).foregroundColor(.white)
                 }
